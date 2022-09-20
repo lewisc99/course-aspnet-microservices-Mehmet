878b6b8e0f1f8a16b5881ee0876260d4246c5eb2
lewisc99 <lewiscontato99@gmail.com>
2022-09-20 16:11:12 -0300

    Play 20. Developing Data Layer - Connect Mongo Docker Container from Catalog.API

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -20,7 +20,7 @@ namespace Catalog.API.Entities
         public string Summary { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
 
     }
 }
