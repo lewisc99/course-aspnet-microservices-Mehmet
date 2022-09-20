d63e99d2009ea680909a7893f1a5c265315b477c
lewisc99 <lewiscontato99@gmail.com>
2022-09-20 14:43:39 -0300

    19. Developing Catalog.API Microservices Creating Entities and MongoDB.Driver Nuget

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId] //Id from MongoDb, like [Id] from relational database
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        [BsonElement("Name")] // like  column name
+        
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public string Price { get; set; }
+
+    }
+}
