fa4f83391fe701187adc15a0de2c990cb6098852
lewisc99 <lewiscontato99@gmail.com>
2022-09-24 16:26:16 -0300

    37. Developing Repository Pattern on Basket.API Microservice

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,19 @@
+﻿using Basket.API.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+
+        Task<ShoppingCart>  GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+
+
+
+    }
+}
