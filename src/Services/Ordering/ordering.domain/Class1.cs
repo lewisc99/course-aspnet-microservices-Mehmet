42a9cb1b29dc0e6bb1017dc123f1bf55015e728b
lewisc99 <lewiscontato99@gmail.com>
2022-10-10 19:34:36 -0300

    90. Developing Ordering.Domain Layer in Clean Architecture

--- src/Services/Ordering/ordering.domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ordering.domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Services/Ordering/ordering.domain/Common/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ordering.domain.Common
+{
+    public abstract class EntityBase
+    {
+
+        public int Id { get; protected set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+    }
+}
