6beb69b4c97735239bc556a06105d2f911e247e0
lewisc99 <lewiscontato99@gmail.com>
2022-10-13 10:04:26 -0300

    99. Developing Ordering.Application Layer - Application Service Registrations

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ordering.application;
 
 namespace Ordering.API
 {
@@ -25,6 +20,10 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            services.AddApplicationServices();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ src/Services/Ordering/ordering.application/ApplicationServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+
+using System.Reflection;
+
+
+namespace ordering.application
+{
+   public static class ApplicationServiceRegistration
+    {
+    
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            
+
+            return services;
+        }
+       
+    }
+}
