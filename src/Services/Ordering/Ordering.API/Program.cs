20632fd9d86aa01d7367f4cfa4e76fc39e9d0fd7
lewisc99 <lewiscontato99@gmail.com>
2022-10-15 17:25:55 -0300

    106. Applying EF.Core Migrations to Sql Server Automatically When Ordering.API

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ordering.infrastructure.Persistence;
+using Ordering.API.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,16 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+             CreateHostBuilder(args).Build()
+             .MigrateDatabase<OrderContext>((context,services) =>
+            {
+                var logger = services.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed
+                .SeedAsync(context, logger)
+                .Wait();
+            })
+            .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
