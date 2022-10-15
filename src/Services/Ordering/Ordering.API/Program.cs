2a318a04f073e67cac9aa4a0951ee65af2a940bf
lewisc99 <lewiscontato99@gmail.com>
2022-10-15 20:39:56 -0300

    108. Test and Run Ordering Microservice

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ordering.infrastructure.Persistence;
 using Ordering.API.Extensions;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API
 {
