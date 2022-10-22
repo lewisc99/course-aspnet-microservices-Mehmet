96d81b9cca2bb9e93d84cd0008446f01516dcea7
lewisc99 <lewiscontato99@gmail.com>
2022-10-22 08:44:01 -0300

    149. Developing Dto Model Class for Api Aggreation Operations

---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Aggregator.Models
+{
+    public class ShoppingModel
+    {
+
+        public string UserName { get; set; }
+
+        public BasketModel basketWithProducts { get; set; }
+
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+
+
+    }
+}
