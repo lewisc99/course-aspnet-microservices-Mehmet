199ec6e075bf7142823a92674d61327ce00eb49b
lewisc99 <lewiscontato99@gmail.com>
2022-10-25 07:09:09 -0300

    164. Developing Index Page in AspnetBasics Shopping Web Application Microservices

--- src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -1,37 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using AspnetRunBasics.Repositories;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Aggregator.Models;
 
 namespace AspnetRunBasics.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICartRepository _cartRepository;
 
-        public IndexModel(IProductRepository productRepository, ICartRepository cartRepository)
+
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public IndexModel(ICatalogService catalogService, IBasketService basketService)
         {
-            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            _catalogService = catalogService;
+            _basketService = basketService;
         }
 
-        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+
+
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            ProductList = await _productRepository.GetProducts();
+            ProductList = await _catalogService.GetCatalog();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string  productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
 
-            await _cartRepository.AddItem("test", productId);
+            var product = await _catalogService.GetCatalog(productId);
+
+            var userName = "swn";
+            var basket = await _basketService.GetBasket(userName);
+
+            basket.Items.Add(new BasketItemModel
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = "Black"
+
+            });
+
+            var basketUpdate = await _basketService.UpdateBasket(basket);
+
             return RedirectToPage("Cart");
         }
     }
